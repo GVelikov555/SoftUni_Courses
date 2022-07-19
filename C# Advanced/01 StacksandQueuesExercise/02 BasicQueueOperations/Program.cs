c800fbbcbe36f6fef5d11d7ed5cc5406582ffd76
G. Velikov <georgi.velikov25@gmail.com>
2022-07-19 12:39:17 +0300

    Solved problem number 1 and 2 about Stacks And Queues Exercise.

---
+++ C# Advanced/01 StacksandQueuesExercise/02 BasicQueueOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> stackOperational = new Queue<int>();
+            int[] info = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int elementsToPush = info[0];
+            int elementsToPop = info[1];
+            int elementToPeek = info[2];
+            int[] numForStack = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < elementsToPush; i++)
+            {
+                stackOperational.Enqueue(numForStack[i]);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                stackOperational.Dequeue();
+            }
+
+            if (stackOperational.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+
+            else if (stackOperational.Contains(elementToPeek))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stackOperational.Min());
+            }
+        }
+    }
+}
