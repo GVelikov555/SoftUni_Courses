71c90c0bcc5a237b6810785c63bbbb1119e894fa
G. Velikov <georgi.velikov25@gmail.com>
2022-07-20 12:23:41 +0300

    Solved problem number 4 about Stacks And Queues Exercise.

---
+++ C# Advanced/01 StacksandQueuesExercise/04 FastFood/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prepairedFood = int.Parse(Console.ReadLine());
+            Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine(ordersQueue.Max());
+
+            while (ordersQueue.Count > 0)
+            {
+                if (prepairedFood >= ordersQueue.Peek())
+                {
+                    prepairedFood -= ordersQueue.Peek();
+                    ordersQueue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+                    break;
+                }
+
+            }
+            if (ordersQueue.Count == 0)
+            {
+                Console.WriteLine($"Orders complete");
+            }
+            
+        }
+    }
+}
