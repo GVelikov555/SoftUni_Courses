9a5f419eaba2353a54d8a04e81be518a25f08b14
G. Velikov <georgi.velikov25@gmail.com>
2022-07-21 12:34:17 +0300

    Solved problem number 5 about Stacks And Queues Exercise.

---
+++ C# Advanced/01 StacksandQueuesExercise/05 FashionBoutique/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pileOfClothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int rackCounter = 0;
+            int rackFreeSpace = rackCapacity;
+
+            while (pileOfClothes.Count > 0)
+            {
+                if (pileOfClothes.Peek() <= rackFreeSpace)
+                {
+                    rackFreeSpace -= pileOfClothes.Peek();
+                    pileOfClothes.Pop();
+                    
+                    if (rackFreeSpace == 0)
+                    {
+                        rackCounter++;
+                        rackFreeSpace = rackCapacity;
+                    }
+                    else if (pileOfClothes.Count == 0)
+                    {
+                        rackCounter++;
+                    }
+                }
+                else
+                {
+                    rackCounter++;
+                    rackFreeSpace = rackCapacity;
+                }
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
