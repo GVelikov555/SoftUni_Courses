6d98b775bb963f4d60e14ce1442b566e6f4fe929
G. Velikov <georgi.velikov25@gmail.com>
2022-07-30 18:36:53 +0300

    Solved problem number 8 about Multidimensional Arrays Exercise.

---
+++ C# Advanced/02 MultidimensionalArraysExercise/08 Bombs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08_Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
