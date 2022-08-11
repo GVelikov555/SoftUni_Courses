7b9bf1638a1e0d95c5c2dcd356603b447806b93f
G. Velikov <georgi.velikov25@gmail.com>
2022-08-11 10:56:31 +0300

    Solved problem number 3 about Sets And Dictionaries Advanced Exercise.

---
+++ C# Advanced/03 SetsandDictionariesAdvancedExercise/03 PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                string input = Console.ReadLine();
+                string[] chElements = input.Split(" ");
+                foreach (string chElement in chElements)
+                {
+                    elements.Add(chElement);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
