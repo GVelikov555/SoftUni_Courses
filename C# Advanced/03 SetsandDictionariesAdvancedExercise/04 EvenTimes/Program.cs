673a3f9c9d1aa830dc350eb0614fc765776e9e46
G. Velikov <georgi.velikov25@gmail.com>
2022-08-12 13:40:24 +0300

    Solved problem number 4 about Sets And Dictionaries Advanced Exercise.

---
+++ C# Advanced/03 SetsandDictionariesAdvancedExercise/04 EvenTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbersOcc = new Dictionary<int, int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numbersOcc.ContainsKey(number))
+                {
+                    numbersOcc.Add(number, 1);
+                }
+                else
+                {
+                    numbersOcc[number]++;
+                }
+            }
+
+            foreach (var entry in numbersOcc)
+            {
+                if (entry.Value % 2 == 0)
+                {
+                    Console.WriteLine(entry.Key);
+                }
+            }
+        }
+    }
+}
